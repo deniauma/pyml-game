58bac7e61cb5635a5b20c2b84a2c0c68d8b09f36
deniauma <mathieu.deniaud@gmail.com>
2016-08-04 09:25:05 +0200

    Update game collision

--- game/explo1.py
+++ game/explo1.py
@@ -1,31 +1,41 @@
 import numpy as np
+import pygame
 from game import Game
 
 
 class Explo1(Game):
-    def __init__(self, grid_size=10):
+    def __init__(self, grid_size=10, pixelsPerBlock=50):
         self.grid_size = grid_size
+        self.pixelsPerBlock = pixelsPerBlock
         self.won = False
         self._actions = {}
         self._actions[0] = "GO_STRAIGHT"
         self._actions[1] = "TURN_LEFT"
         self._actions[2] = "TURN_RIGHT"
-        self._robotPos = np.array((5, 5))
+        self._robotPos = np.array((1, 5)) * pixelsPerBlock
         self._robotDir = np.array((0, -1))
-        self._robotSpeed = 1
-        self._visionDistance = 3
+        self._robotSpeed = 0.1 * pixelsPerBlock
+        self._visionDistance = 3.
         self.reset()
 
     def reset(self):
         self._state = np.zeros((self.grid_size, self.grid_size))
-        self._state[2][4] = 1
-        self._state[2][5] = 1
-        self._state[2][6] = 1
+        # self._state[2][3] = 1
+        self._state[3][3] = 1
+        self._state[4][3] = 1
         self._state[6][2] = 1
         self._state[6][3] = 1
+        # self._state[2][5] = 1
+        # self._state[3][5] = 1
+        self._state[3][6] = 1
         self._state[9][7] = 1
         self._state[9][8] = 1
 
+        for i in range(self.grid_size):
+            self._state[0][i] = 1
+            self._state[self.grid_size-1][i] = 1
+            self._state[i][0] = 1
+            self._state[i][self.grid_size - 1] = 1
 
     @property
     def name(self):
@@ -54,18 +64,24 @@ class Explo1(Game):
 
     def checkNoCollision(self, move):
         newPos = self._robotPos + move
-        if self._state[newPos[0]][newPos[1]] == 1:
-            return False
+        collision = False
+        robotRect = pygame.Rect(newPos, [self.pixelsPerBlock, self.pixelsPerBlock])
+        for i in range(len(self._state)):
+            for j in range(len(self._state[i])):
+                if self._state[i][j] == 1:
+                    blockrect = pygame.Rect([[i*self.pixelsPerBlock, j*self.pixelsPerBlock], [self.pixelsPerBlock, self.pixelsPerBlock]])
+                    if robotRect.colliderect(blockrect):
+                        return False
         return True
 
     def moveRobot(self, action):
         move = np.array((0,0))
         if self._actions[action] == "GO_STRAIGHT":
-            move = self._robotDir * self._robotSpeed
+            move = self._robotDir * int(self._robotSpeed)
         elif self._actions[action] == "TURN_LEFT":
             self.rotateRobot(10)
         elif self._actions[action] == "TURN_RIGHT":
             self.rotateRobot(-10)
 
         if self.checkNoCollision(move):
-            self._robotPos += move
+            self._robotPos += move.astype(int)
--- testExplo.py
+++ testExplo.py
@@ -18,29 +18,43 @@ displayGrid = True
 game = Explo1(grid_size)
 
 
+def drawGrid(grid_size, prixelsPerBlock):
+    for i in range(grid_size):
+        startH = np.array((0, pixelsPerBlock)) * i
+        stopH = np.array((grid_size * pixelsPerBlock, pixelsPerBlock)) * i
+        startV = np.array((pixelsPerBlock, 0)) * i
+        stopV = np.array((pixelsPerBlock, pixelsPerBlock * pixelsPerBlock)) * i
+        pygame.draw.line(screen, WHITE, startH, stopH)
+        pygame.draw.line(screen, WHITE, startV, stopV)
+
+
+def drawMap(map):
+    for i in range(len(map)):
+        for j in range(len(map[i])):
+            if map[i][j] == 1:
+                start = np.array((i, j)) * pixelsPerBlock
+                dim = [pixelsPerBlock, pixelsPerBlock]
+                pygame.draw.rect(screen, YELLOW, [start, dim])
+
+
 while 1:
     pygame.time.wait(100)
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
     robotPos, robotDir, state = game.getGameData()
-    print robotDir
-    pos = robotPos * pixelsPerBlock
-
+    # pos = np.array((0,0))
+    # pos[0] = int(robotPos[0] * pixelsPerBlock)
+    # pos[1] = int(robotPos[1] * pixelsPerBlock)
 
     screen.fill(BLACK)
     if displayGrid:
-        for i in range(grid_size):
-            startH = np.array((0, pixelsPerBlock)) * i
-            stopH = np.array((grid_size*pixelsPerBlock, pixelsPerBlock)) * i
-            startV = np.array((pixelsPerBlock, 0)) * i
-            stopV = np.array((pixelsPerBlock, pixelsPerBlock * pixelsPerBlock)) * i
-            pygame.draw.line(screen, WHITE, startH, stopH)
-            pygame.draw.line(screen, WHITE, startV, stopV)
-    game.play(1)
-    pygame.draw.circle(screen, RED, pos + pixelsPerBlock/2, pixelsPerBlock/2)
-    startDir = pos + pixelsPerBlock/2
+        drawGrid(grid_size, pixelsPerBlock)
+    drawMap(state)
+    game.play(random.randrange(3))
+    pygame.draw.circle(screen, RED, robotPos + pixelsPerBlock/2, pixelsPerBlock/2)
+    startDir = robotPos + pixelsPerBlock/2
     stopDir = startDir + robotDir*pixelsPerBlock
     pygame.draw.line(screen, RED, startDir, stopDir)
-    # pygame.draw.rect(screen, mouseColor, [mousePos,mouseDim])
     pygame.display.flip()
+
